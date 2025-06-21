a6b7ac7629e56defe0601d2fbd777d6dbef6b66a
gmeury <gerico@jgwill.com>
2025-06-21 00:30:27 -0400

    Refactor del-key.sh: replace hardcoded URL with environment variable for KV_REST_API_URL
    Create a branch for Musebase integration #5

--- scripts/del-key.sh
+++ scripts/del-key.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ../.env
-curl -X DELETE "https://loyal-lamb-40648.upstash.io/del/$1" \
+curl -X DELETE "$KV_REST_API_URL/del/$1" \
   -H "Authorization: Bearer $KV_REST_API_TOKEN"
